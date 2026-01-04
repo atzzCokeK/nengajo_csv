945677d209b9d70d5fa8d01ac58ec02756b9c55c
Atsushi Kawamura <atsushi1230.sdi@gmail.com>
2026-01-04 17:34:56 +0900

    Add HeaderAnalyzer service to analyze CSV headers against a template

---
+++ app/services/csv/header_analyzer.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module CSV
+  class HeaderAnalyzer
+    def initialize(headers:, template:)
+      @headers = headers
+      @template = template
+    end
+
+    def analyze
+      mapped = {}
+      mapping = @template[:mapping]
+
+      @headers.each do |header|
+        mapped[header] = mapping[header] if mapping.key?(header)
+      end
+
+      expected = mapping.values.uniq
+
+      {
+        mapped: mapped,
+        missing: expected - mapped.values,
+        extra: @headers - mapping.keys
+      }
+    end
+  end
+end
